265408ea5ac5642dd2b2baf6da9fb2f06c764d95
John Clema <john.clema@gmail.com>
2015-06-18 21:06:45 +0800

    Feat: Running locally with grunt pushing with Rakefile

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-bower-task");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-exec");
+
+  grunt.initConfig({
+    copy: {
+      jquery: {
+        files: [{
+          expand: true,
+          cwd: "public/bower_components/jquery/dist/",
+          src: "jquery.min.js",
+          dest: "vendor/js/"
+        }]
+      },
+      materialize: {
+        files: [{
+          expand: true,
+          cwd: "public/bower_components/materialize/dist/css/",
+          src: "materialize.min.css",
+          dest: "vendor/css/"
+        },
+        {
+          expand: true,
+          cwd: "public/bower_components/materialize/dist/js/",
+          src: "materialize.min.js",
+          dest: "vendor/js/"
+        }]
+      }
+    },
+    exec: {
+      jekyll: {
+        cmd: "jekyll build --trace"
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      source: {
+        files: [
+          "_drafts/**/*",
+          "_includes/**/*",
+          "_layouts/**/*",
+          "_posts/**/*",
+          "css/**/*",
+          "js/**/*",
+          "_config.yml",
+          "*.html",
+          "*.md"
+        ],
+        tasks: [
+          "exec:jekyll"
+        ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: '_site',
+          livereload: true
+        }
+      }
+    }
+  });
+  grunt.registerTask("build", ["copy", "exec:jekyll"]);
+
+  grunt.registerTask("serve", ["build","connect:server","watch"]);
+
+  grunt.registerTask("default", ["serve"]);
+};
