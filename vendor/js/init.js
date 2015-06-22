273612ff7b2b2d03c99eceb382ea68e089b150b9
John Clema <john.clema@gmail.com>
2015-06-22 14:30:34 +0800

    fix/refactor: Logo now sizing well for mobile and desktop, sccs code cleanup

--- vendor/js/init.js
+++ vendor/js/init.js
@@ -3,6 +3,13 @@
 
     $('.button-collapse').sideNav();
     $('select').material_select();
+    var options = [
+    {selector: '#staggered-test', offset: 50, callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )' },
+    {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
+    {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+    {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
+  ];
+  Materialize.scrollFire(options);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
